ce6b16aec10c29cd6170bb029174fd720bc485c5
you@examasdasple.com <you@exasdample.com>
2024-02-20 14:44:34 +0000

    asdasd

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  * Description: 'the program's description'
- * @parameter: describe the parameter
+ * parameter: describe the parameter
  * Return: Always 0 (Success)
  */
 int main(void)
