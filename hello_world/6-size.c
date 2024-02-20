01f848551975d0c9eb2330babc87f00607061191
you@examasdasple.com <you@exasdample.com>
2024-02-20 15:43:16 +0000

    asdasd

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %d\n",sizeof(char*));
-printf("\nSize of an int: %d\n",sizeof(int*));
-printf("\nSize of a long int: %d\n",sizeof(long int*));
-printf("\nSize of a long long int: %d\n" ,sizeof(long long int*));
-printf("\nSize of a float: %d\n" ,sizeof(float*));
+printf("Size of a char: %d\n",sizeof(char));
+printf("\nSize of an int: %d\n",sizeof(int));
+printf("\nSize of a long int: %d\n",sizeof(long int));
+printf("\nSize of a long long int: %d\n" ,sizeof(long long int));
+printf("\nSize of a float: %d\n" ,sizeof(float));
 return (0);
 }
