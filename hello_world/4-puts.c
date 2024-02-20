c220126ca80d2938b34f06406cb3c6961c72bde0
you@examasdasple.com <you@exasdample.com>
2024-02-20 14:43:01 +0000

    asdasd

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
-*ASDASD
-*ASDASDAS
-*/
-int main(void)/*MAIN FUNC*/
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 char *str1 = "\"Programming is like building a multilingual puzzle";
 puts(str1);
 return (0);
 }
-/*
-*GRGR
-*HTHT
-*/
