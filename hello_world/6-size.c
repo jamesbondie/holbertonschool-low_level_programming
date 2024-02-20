0f27f56257eb8478969273dd1c712c41b532ce96
you@examasdasple.com <you@exasdample.com>
2024-02-20 15:07:34 +0000

    asdasd

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,12 +7,7 @@
  */
 int main(void)
 {
-printf("sizeof(int)       = %d\n",sizeof(int));
-printf("sizeof(float)     = %d\n",sizeof(float));
-printf("sizeof(double)    = %d\n",sizeof(double));
-printf("sizeof(char)      = %d\n",sizeof(char));
-printf("sizeof(167)       = %d\n",sizeof(167));
-printf("sizeof(3.1415926) = %d\n",sizeof(3.1415926));
-printf("sizeof('$')       = %d\n",sizeof('$'));
+printf("sizeof(int) = %d\n",sizeof(int));
+printf("sizeof(float) = %d\n",sizeof(float));
 return (0);
 }
