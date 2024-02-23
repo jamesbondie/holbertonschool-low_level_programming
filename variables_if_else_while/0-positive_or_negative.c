3b490dd76bcd70eb974cdf43dfb319529530ef41
Your Name <you@example.com>
2024-02-23 03:35:08 +0000

    asd

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,12 @@
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
-/* betty style doc for function main goes there */
+/**
+ * main - Entry poin
+ * Description: 'the program's description'
+ * @parameter: describe the parameter 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
