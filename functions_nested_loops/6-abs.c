4f52262857574f072839e7aa5c09bd6c2c92d650
Your Name <you@example.com>
2024-02-24 19:25:23 +0000

    asdasd

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+/*
+ * asdasdasdc
+ * SAsdasdwadn
+ */
+
+#include "main.h"
+
+/**
+ * _abs - dasdasdasdasdeasd
+ * @j: asdasdasdasdasdasdasdadsdasd
+ *
+ * Return: dasdasdasdrwiseasdasdasdas
+ */
+int _abs(int j)
+{
+if (j >= 0)
+j = j;
+if (i < 0)
+j = -j;
+return (j);
+}
