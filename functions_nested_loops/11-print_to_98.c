d9744f4a3bbbf2174db3341fef2874c9d81e0d96
Your Name <you@example.com>
2024-02-24 20:03:59 +0000

    asdasd

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+/*
+ * asdasdasdc
+ * SAsdasdwadn
+ */
+
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - dasdasdjaslkjdlajskldjalk
+ * @a: - asdfsdfasdfasfdsfdsf
+ * @b: - asdasdasjdhakshddasdasd
+ * Return: - asdasdasdasdasdasdasdasd
+ */
+void print_to_98(int a)
+{
+if (a >= 98)
+{
+while (a > 98)
+printf("%d, ", a--);
+printf("%d\n", a);
+}
+else
+{
+while (a < 98)
+printf("%d, ", a++);
+printf("%d\n", a);
+}
+}
+
