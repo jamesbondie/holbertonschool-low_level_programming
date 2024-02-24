4d088f96ef1dc93be9a45a8f173f41610ec7fc6b
Your Name <you@example.com>
2024-02-24 19:21:24 +0000

    asdasd

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+/*
+ * asdasdasdc
+ * SAsdasdwadn
+ */
+
+#include "main.h"
+
+/**
+ * print_sign - dasdasdasdasdeasd
+ * @c: asdasdasdasdasdasdasdadsdasd
+ *
+ * Return: dasdasdasdrwiseasdasdasdas
+ */
+int print_sign(int c)
+{
+if (c > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (c == 0)
+{
+_putchar('0');
+return (0);
+}
+else if (c < 0)
+{
+_putchar('-');
+return (-1);
+}
+return (0);
+}
