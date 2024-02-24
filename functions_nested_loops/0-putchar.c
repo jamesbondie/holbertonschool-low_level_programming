bf8002884eb54be36dda55072bf6ab420b6d6335
Your Name <you@example.com>
2024-02-24 16:53:12 +0000

    asdasd

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase, then in uppercase,
+ *              followed by the character '$'.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("_putchar");
+return (0);
+}
