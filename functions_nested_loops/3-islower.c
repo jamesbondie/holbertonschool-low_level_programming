8e9a52db9d8c466b3e2a38018c9b510d01962c2d
Your Name <you@example.com>
2024-02-24 18:56:18 +0000

    asdasd

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+/*
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+#include "main.h"
+
+/**
+ * _islower - bery good my func
+ * print_alphabet_x10 - is nice one really good one@L asd:w
+ * _putchar - very high conversation topic
+ * print_alphabet is a good function ahsdbjasdjashjdajskhdka  aksjhdak
+ * print_alphabet_x10 is a good function
+ */
+int _islower(int c);
+{
+int j;
+r = _islower('c');
+_putchar(r + '0');
+_putchar('\n');
+}
