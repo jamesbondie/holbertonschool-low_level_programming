6921efec54d9494021e79d7de125e9e384b67251
Your Name <you@example.com>
2024-02-24 15:08:40 +0000

    asdas

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase, then in uppercase,
+ *              followed by the character '$'.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c = '0';
+while (c <= '9')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
