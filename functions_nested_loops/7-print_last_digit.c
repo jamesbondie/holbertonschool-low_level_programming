91f4d13d374d78acfcbee9320472ad85601a56df
Your Name <you@example.com>
2024-02-24 19:32:30 +0000

    asdasd

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+/*
+ * asdasdasdc
+ * SAsdasdwadn
+ */
+
+#include "main.h"
+
+/**
+ * print_last_digit - dasdasdasdasdeasd
+ * @k: asdasdasdasdasdasdasdadsdasd
+ *
+ * Return: dasdasdasdrwiseasdasdasdas
+ */
+int print_last_digit(int j);
+{
+int k = j % 10;
+if (k < 0)
+k = -k;
+_putchar(k + '0');
+return (k);
+}
