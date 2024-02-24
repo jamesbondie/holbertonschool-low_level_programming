5787b6ecebd142a5376f8e34ca7455f6e5194596
Your Name <you@example.com>
2024-02-24 19:54:56 +0000

    asdasd

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+/*
+ * asdasdasdc
+ * SAsdasdwadn
+ */
+
+#include "main.h"
+
+/**
+ * jack_bauer - dasdasdjaslkjdlajskldjalk
+ * Return: dasdasdasdrwiseasdasdasdas
+ * times_table - asdfsdfasdfasfdsfdsf
+ *
+ * Return: Nothing.
+ */
+void times_table(void)
+{
+int b = 0, c, d;
+while (b <= 9)
+{
+c = 0;
+while (c <= 9)
+{
+d = (b * c);
+if (c != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+if (d >= 10)
+{
+_putchar((d / 10) + '0');
+_putchar((d % 10) + '0');
+}
+else if (d < 10 && c != 0)
+{
+_putchar(' ');
+_putchar((d % 10) + '0');
+}
+else
+_putchar((d % 10) + '0');
+c++;
+}
+_putchar('\n');
+b++;
+}
+}
