5cac989deb80ab211c8f6b30b7869c9ad337b197
Your Name <you@example.com>
2024-02-24 18:36:28 +0000

    jlkjk

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+/*
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - is nice one really good one@L asd:w
+ * _putchar - very high conversation topic
+ * print_alphabet is a good function ahsdbjasdjashjdajskhdka  aksjhdak
+ * print_alphabet_x10 is a good function
+ */
+void print_alphabet_x10(void)
+{
+i = 48
+while (i <= 58)
+{
+char i = 'a';
+while (i <= 'z')
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}
+i++;
+}
