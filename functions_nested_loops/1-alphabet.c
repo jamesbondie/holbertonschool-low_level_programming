b16bd0022266aa0f58f82149b5d1d8d889eb280f
Your Name <you@example.com>
2024-02-24 18:25:04 +0000

    jlkjk

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
-/**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
+/*
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
 #include "main.h"
+
 /**
-* print_alphabet is a good function
-*/
+ * print_alphabet is a good function
+ */
 void print_alphabet(void)
 {
 char i = 'a';
