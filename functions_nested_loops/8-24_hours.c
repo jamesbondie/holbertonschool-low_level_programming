c2a7257b8c78430ce26011e25c848e45b5bfd7fd
Your Name <you@example.com>
2024-02-24 19:46:27 +0000

    asdasd

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+/*
+ * asdasdasdc
+ * SAsdasdwadn
+ */
+
+#include "main.h"
+
+/**
+ * jack_bauer - dasdasdasdasdeasd
+ * @c: asdkaskdjaslkjdlkasjldkaj
+ * @o: sadasjdlkasjdklajskldjalk
+ * Return: dasdasdasdrwiseasdasdasdas
+ */
+void jack_bauer(void)
+{
+int c, o;
+c = 0;
+while (c < 24)
+{
+o = 0;
+while (o < 60)
+{
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+_putchar(':');
+_putchar((o / 10) + '0');
+_putchar((o % 10) + '0');
+_putchar('\n');
+o++;
+}
+c++;
+}
+}
+}
