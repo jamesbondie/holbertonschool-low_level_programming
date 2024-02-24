70ff40920f139ff8989ef14dff94ed9d1fe79695
Your Name <you@example.com>
2024-02-24 19:15:28 +0000

    asdasd

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+/*
+ * asdasdasdc
+ * SAsdasdwadn
+ */
+
+#include "main.h"
+
+/**
+ * _isalpha - dasdasdasdasdeasdcksa 
+ * @c: asdasdasdasdasdasdasdadsdasd
+ *
+ * Return: dasdasdasdrwiseasdasdasdas
+ */
+int _isalpha(int c)
+{
+if ((c <= 'Z' && c >= 'A') ||
+(c <= 'z' && c >= 'a'))
+return (1);
+else
+return (0);
+}
