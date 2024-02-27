bb3f38b09b3b531434b298acd6da89b0b082aaba
Your Name <you@example.com>
2024-02-27 07:01:55 +0000

    asdasd

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+/*
+ * main - Entry point
+ * Description: 'the program's description'
+ * @parameter: descri
+ * * Return: Always 0 (Success)
+ */
+#include "main.h"
+/**
+ * print_line - dasdasdasdasdeasd
+ * @n: asdasdasdasdasdasdasdadsdasd
+ *  adlskdjalksdjalksjdlaksjdlkas
+ * Return: dasdasdasdrwiseasdasdasdas
+ */
+void print_diagonal(int n)
+{
+int i;
+i = 0;
+while (i < n && n > 0)
+{
+_putchar(92);
+i++;
+}
+_putchar('\n');
+}
