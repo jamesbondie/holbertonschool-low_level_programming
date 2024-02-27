edd4e5f09dcaa5e6b7b2a78b8eabdb0eb147f5fb
Your Name <you@example.com>
2024-02-27 06:04:01 +0000

    asdasda

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,29 @@
+/*
+ * main - Entry point
+ * Description: 'the program's description'
+ * @parameter: descri
+ * * Return: Always 0 (Success)
+ */
+#include "main.h"
+/**
+ * _isupper - dasdasdasdasdeasd
+ * @c: asdasdasdasdasdasdasdadsdasd
+ *
+ * Return: dasdasdasdrwiseasdasdasdas
+ */
+int _isdigit(int c)
+/*
+ * _isdigit - super function really
+ * @c: aksdjlaksdjlkasd
+ * Return: kasljdlkasjdkalsjdklasjdlk
+ */
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
