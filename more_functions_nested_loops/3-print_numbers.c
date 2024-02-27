f3f0591c6679825b077d273a27a8fec833b848b3
Your Name <you@example.com>
2024-02-27 06:12:43 +0000

    asdasda

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+/*
+ * main - Entry point
+ * Description: 'the program's description'
+ * @parameter: descri
+ * * Return: Always 0 (Success)
+ */
+#include "main.h"
+/**
+ * print_numbers - dasdasdasdasdeasd
+ * @a: asdasdasdasdasdasdasdadsdasd
+ * @b: adlskdjalksdjalksjdlaksjdlkas
+ * Return: dasdasdasdrwiseasdasdasdas
+ */
+void print_numbers(void);
+{
+int a;
+a = 48;
+while (a < 57)
+{
+_putchar(a);
+a++;
+}
