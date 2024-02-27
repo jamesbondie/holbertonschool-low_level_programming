c375625710fce328d56f33dda9728fd7f3b4f736
Your Name <you@example.com>
2024-02-27 18:44:45 +0000

    asdasdasd

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,35 @@
+/*
+ * main - Entry point
+ * Description: 'the program's description'
+ * @parameter: descri
+ * * Return: Always 0 (Success)
+ */
+#include "main.h"
+/**
+ * print_square - dasdasdasdasdeasd
+ * @size: asdasdasdasdasdasdasdadsdasd
+ *  adlskdjalksdjalksjdlaksjdlkas
+ * Return: dasdasdasdrwiseasdasdasdas
+ */
+void print_square(int size)
+{
+int x = 0;
+int i = 0;
+if (size > 0)
+{
+while (x < size) {
+while (i <= size) {
+_putchar(35);
+i++;
+}
+i=0;
+_putchar('\n');
+x++;
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n')
+}
+}
