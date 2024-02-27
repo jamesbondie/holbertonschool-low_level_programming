de333d0197b68a1558d44a1dc0177a40a7a5870f
Your Name <you@example.com>
2024-02-27 06:54:44 +0000

    asdasd

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+/*
+ * main - Entry point
+ * Description: 'the program's description'
+ * @parameter: descri
+ * * Return: Always 0 (Success)
+ */
+#include "main.h"
+/**
+ * print_line - dasdasdasdasdeasd
+ * @n: asdasdasdasdasdasdasdadsdasd
+ *  adlskdjalksdjalksjdlaksjdlkas
+ * Return: dasdasdasdrwiseasdasdasdas
+ */
+void print_line(int n)
+{
+int i;
+i=0;
+int n = 0;
+while (i <= n && n > 0)
+{
+_putchar(95);
+i++;
+}
+_putchar('a');
+return 0;
+}
