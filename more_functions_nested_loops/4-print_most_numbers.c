bff2606578e16746f592adb505971f7ef52cc037
Your Name <you@example.com>
2024-02-27 06:22:10 +0000

    asdasd

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,28 @@
+/*
+ * main - Entry point
+ * Description: 'the program's description'
+ * @parameter: descri
+ * * Return: Always 0 (Success)
+ */
+#include "main.h"
+/**
+ * print_most_numbers - dasdasdasdasdeasd
+ * asdasdasdasdasdasdasdadsdasd
+ *  adlskdjalksdjalksjdlaksjdlkas
+ * Return: dasdasdasdrwiseasdasdasdas
+ */
+void print_most_numbers(void)
+{
+int a;
+a = 48;
+while (a <= 57)
+{
+_putchar(a);
+a++;
+if (a == 2 || a == 4)
+{
+a++;
+}
+}
+_putchar('\n');
+}
