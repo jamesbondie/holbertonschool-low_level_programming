d2461f47748e11b8905d59730f0fa6f2b6dfe891
Your Name <you@example.com>
2024-02-27 05:55:27 +0000

    asdas

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,10 @@
 /**
- * main gh blalalabdlabslkdnalk
+ *  main - Entry point
+ * 
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * 
+ * Return: Always 0 (Success)
  */
 #include "main.h"
 /**
@@ -8,6 +13,11 @@
  * Return: kasljdlkasjdkalsjdklasjdlk
  */
 int _isupper(int c)
+/**
+ * _isupper - super function really
+ * @c: aksdjlaksdjlkasd
+ * Return: kasljdlkasjdkalsjdklasjdlk
+ */
 {
 if (c >= 'A' && c <= 'Z')
 {
