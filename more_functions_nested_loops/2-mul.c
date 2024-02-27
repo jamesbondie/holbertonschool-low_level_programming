1817bbc958e0e5123a2a5bbf2e5744169dffda06
Your Name <you@example.com>
2024-02-27 06:08:22 +0000

    asdasda

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/*
+ * main - Entry point
+ * Description: 'the program's description'
+ * @parameter: descri
+ * * Return: Always 0 (Success)
+ */
+#include "main.h"
+/**
+ * mul - dasdasdasdasdeasd
+ * @c: asdasdasdasdasdasdasdadsdasd
+ *
+ * Return: dasdasdasdrwiseasdasdasdas
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
+}
