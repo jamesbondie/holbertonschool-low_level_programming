316bb07c4e607c0c876153704af8794af6dbda4e
Your Name <you@example.com>
2024-02-27 06:01:31 +0000

    asdasda

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,15 @@
-/**
- *  main - Entry point
+/*
+ * main - Entry point
  * Description: 'the program's description'
  * @parameter: descri
  * * Return: Always 0 (Success)
  */
 #include "main.h"
-/*
- * _isupper - super function really
- * @c: aksdjlaksdjlkasd
- * Return: kasljdlkasjdkalsjdklasjdlk
+/**
+ * _isupper - dasdasdasdasdeasd
+ * @c: asdasdasdasdasdasdasdadsdasd
+ *
+ * Return: dasdasdasdrwiseasdasdasdas
  */
 int _isupper(int c)
 /*
