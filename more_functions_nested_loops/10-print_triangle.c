92e0fa3d57500805439308dc5ec4ab7b7b2e4af3
Your Name <you@example.com>
2024-02-28 19:15:27 +0000

    asdasdas

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * print_triangle - askdljaskldjalksjlqkjwldkjlasd
-* asdasdasdasdasdasdasd
+* @size: asdasdasdasdasdasdasd
 *  asdasdaksdjalksd
 *  askdajslkdjalksjdlaksjd
 * skdjklfajlkdjf
