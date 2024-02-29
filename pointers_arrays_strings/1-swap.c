1986fbbe55687bd0b434cd32763367c034708850
Your Name <you@example.com>
2024-02-29 19:40:40 +0000

    asdasd

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - good asdasdasdasda
+ * @a: aisjdaisjdalksjdlkasjdkl
+ * @b: askjdkalksjdkaljsdlkaskd
+ */
+void swap_int(int *a, int *b)
+{
+int *t;
+*t = *a;
+*a = *b;
+*b = *t;
+}
