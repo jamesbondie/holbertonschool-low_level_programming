1cd4e7caa2c00353a4169f9cd45c5d337937792e
Your Name <you@example.com>
2024-02-29 19:16:10 +0000

    asdasd

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,6 @@
 #include "main.h"
-/*reset_to_98 - good asdasdasdasda
- * @*n: aisjdaisjdalksjdlkasjdkla
- *
- *
+/** reset_to_98 - good asdasdasdasda
+ * @*n: aisjdaisjdalksjdlkasjdkl
  */
 void reset_to_98(int *n)
 {
