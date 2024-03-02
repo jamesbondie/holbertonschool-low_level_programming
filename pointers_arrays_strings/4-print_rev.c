7cd560d04c5f6135915d5833399d21a75a26fe58
Your Name <you@example.com>
2024-03-02 08:24:53 +0000

    asdasd

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - good asdasdasdasda
+ * @s: aisjdaisjdalksjdlkasjdkl
+ * askjdkalksjdkaljsdlkaskd
+ */
+void print_rev(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+while (i >= 0)
+{
+_putchar(s[i]);
+i--;
+}
+_putchar('\n');
+}
