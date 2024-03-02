4ea7e8a862c743baa67988f94299b16732135ef2
Your Name <you@example.com>
2024-03-02 15:45:26 +0000

    asdasd

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - good asdasdasdasda
+ * @str: aisjdaisjdalksjdlkasjdkl
+ * askjdkalksjdkaljsdlkaskd
+ */
+void puts2(char *str)
+{
+int i;
+i = 0;
+while (str[i] != '\0')
+{
+i=i+2;
+_putchar(str[i]);
+}
+_putchar('\n');
+}
