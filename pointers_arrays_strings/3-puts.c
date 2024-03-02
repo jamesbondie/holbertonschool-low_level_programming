0abefe1bdbcfa0f72911324af3a68e534dd1a8e4
Your Name <you@example.com>
2024-03-02 08:20:57 +0000

    asdasd

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - good asdasdasdasda
+ * @str: aisjdaisjdalksjdlkasjdkl
+ * askjdkalksjdkaljsdlkaskd
+ */
+void _puts(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
