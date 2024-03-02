02bac0b93657f5a5d411c1aaf3d400f7a1221085
Your Name <you@example.com>
2024-03-02 21:30:31 +0000

    asdasd

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,28 +1,25 @@
 #include "main.h"
 /**
- * cap_leet - good asdasdasdasda
+ * leet - good asdasdasdasda
  * @s: aisjdaisjdalksjdlkasjdkl
- * askjdkalksjdkaljsdlkaskd
+ * Return: askjdkalksjdkaljsdlkaskd
  * alskdaskldaklsdkalsdaks
  * alksjdalksjdlkasjdlkajkl
  */
-char *leet(char *t)
+char *leet(char *)
 {
-int p = 0;
-if (t[0] >= 'a' && t[0] <= 'z')
-t[0] -= 32;
-while (t[p] != '\0')
+char *r = s;
+char c[] = { 4, 3, 0, 7, 1 };
+char x[] = { 'a', 'e', 'o', 't', 'l' };
+int i = 0;
+while (*s)
 {
-if (t[p] == ' ' || t[p] == '\t' || t[p] == '\n'
-|| t[p] == ',' || t[p] == ';' || t[p] == '.'
-|| t[p] == '.' || t[p] == '!' || t[p] == '?'
-|| t[p] == '"' || t[p] == '(' || t[p] == ')'
-|| t[p] == '{' || t[p] == '}')
+for (i = 0; i < 5; i++)
 {
-if (t[p + 1] >= 'a' && t[p + 1] <= 'z')
-t[p + 1] -= 32;
+if (*s == x[i] || *s == x[i] - 32)
+*s = c[i] + '0';
 }
-p++;
+s++;
 }
-return (t);
+return (r);
 }
