29feab3f61a6fac12376841db951396fee41b915
Your Name <you@example.com>
2024-03-02 21:48:16 +0000

    asdasd

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - adsfasdfsadfasdfa
+ * @name: asdfasdfasdfasdfdog
+ * @age: aasdfasog
+ * @owner: nasdfasdfasdf
+ */
 typedef struct dog
 {
 char *name;
