ad8fe125666c1b9a9ccf451ac35d47ddc22425c0
Your Name <you@example.com>
2024-03-02 19:27:01 +0000

    asdasd

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strncat - good asdasdasdasda
+ * @dest: aisjdaisjdalksjdlkasjdkl
+ * @src: askjdkalksjdkaljsdlkaskd
+ * @n: alskdaskldaklsdkalsdaks
+ * Return: alksjdalksjdlkasjdlkajkl
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int lend = 0;
+int lens = 0;
+while (dest[lend])
+{
+	lend++;
+}
+while (src[lens] > n)
+{
+
+dest[lend+lens] = src[lens];
+
+
+
+lens++;
+}
+
+
+
+
+return (dest);
+}
