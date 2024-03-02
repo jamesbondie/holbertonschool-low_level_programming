7ac5742865bb4eaa46bcd98bc5fc8981e200a26d
Your Name <you@example.com>
2024-03-02 14:53:36 +0000

    asdasd

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,7 +8,7 @@ void rev_string(char *s)
 {
 int i;
 int a;
-char b[i];
+char *b;
 i = 0;
 a = 0;
 while (s[i] != '\0')
@@ -16,7 +16,6 @@ while (s[i] != '\0')
 i++;
 }
 i--;
-
 while (i >= 0)
 {
 i--;
