1329c43829de7c11939885d610d76ea931710aee
Your Name <you@example.com>
2024-03-02 15:56:39 +0000

    asdasd

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -7,14 +7,14 @@
 void puts_half(char *str)
 {
 int len = 0;
-int n = ( len - 1 ) / 2;
+int n;
 int a = 0;
 while (str[len] != '\0')
 {
 	len++;
 }
 len--;
-
+n = ( len - 1 ) / 2;
 while (n <= len)
 {
 	_putchar(str[a]);
