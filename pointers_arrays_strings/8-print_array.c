c23ae93aabf2801a7bdf678ae58bc866d97edf99
Your Name <you@example.com>
2024-03-02 16:23:02 +0000

    asdasd

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_array - good asdasdasdasda
+ * @a: aisjdaisjdalksjdlkasjdkl
+ * @n: askjdkalksjdkaljsdlkaskd
+ */
+void print_array(int *a, int n)
+{
+int i = 0;
+while (n - 2 >= i)
+{
+	printf("%d, ", a[i]);
+	i++;
+}
+printf("%d", a[i]);
+_putchar('\n');
+}
