2a99186aa3900841f23ee6495a1f81d72f1c11db
Your Name <you@example.com>
2024-03-02 22:37:08 +0000

    asdasd

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,7 +9,8 @@
  * @owner: asdasdasdasdasdag
  * Return: ajsdfnakjsdnfkajsndkfljasn
  */
-dog_t *new_dog(char *name, float age, char*owner) {
+dog_t *new_dog(char *name, float age, char *owner)
+{
 dog_t *i;
 int name_len, owner_len;
 int j;
@@ -22,17 +23,21 @@ for (name_len = 0; name[name_len] != '\0'; name_len++);
 for (owner_len = 0; owner[owner_len] != '\0'; owner_len++);
 i->name = malloc(name_len + 1);
 i->owner = malloc(owner_len + 1);
-if (i->name == NULL || i->owner == NULL) {
+if (i->name == NULL || i->owner == NULL)
+{
 free(i->name);
 free(i->owner);
 free(i);
 return NULL;
 }
 for (j = 0; j <= name_len; j++)
+{
 i->name[j] = name[j];
+}
 for (j = 0; j <= owner_len; j++)
+{
 i->owner[j] = owner[j];
 i->age = age;
 return i;
 }
-
+}
