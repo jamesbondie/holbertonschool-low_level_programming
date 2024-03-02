3e97882bce2d7a99972dd165c440d183a79f47d1
Your Name <you@example.com>
2024-03-02 20:52:00 +0000

    asdasd

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * string_toupper- good asdasdasdasda
+ * @a: aisjdaisjdalksjdlkasjdkl
+ * @n: askjdkalksjdkaljsdlkaskd
+ * alskdaskldaklsdkalsdaks
+ * alksjdalksjdlkasjdlkajkl
+ */
+char *string_toupper(char *)
+{
+int len = 0;
+
+while (*[len])
+{
+if (*[len] >= 'a' && *[len] <= 'z') {
+        *[len] = *[len] - 'a' + 'A';
+    }
+}
+
+
+
+
+
+
+	return (*);
+}
+
