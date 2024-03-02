7316f7af211b6244d6a47fe7a6bfd39696df9a19
Your Name <you@example.com>
2024-03-02 08:15:02 +0000

    asdasd

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - good asdasdasdasda
+ * @s: aisjdaisjdalksjdlkasjdkl
+ * Return: askjdkalksjdkaljsdlkaskd
+ */
+int _strlen(char *s)
+{
+int i = 0;
+while(s[i]!='\0')
+{
+i++;
+}
+return (i);
+}
