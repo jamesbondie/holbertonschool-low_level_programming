e22df6a3dfdc969850b725a71a086a9cc2c5371b
Your Name <you@example.com>
2024-03-02 17:41:21 +0000

    asdasd

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcpy - good asdasdasdasda
+ * @dest: aisjdaisjdalksjdlkasjdkl
+ * @src: askjdkalksjdkaljsdlkaskd
+ * Return: alksjdalksjdlkasjdlkajkl
+ */
+int _atoi(char *s)
+{
+int i = 0;
+while (s[i])
+{
+	len++;
+}
+while (len > i)
+{
+if (s[i] >= '0' && s[i] <= '9')
+{
+_putchar(s[i]);
+i++;
+}
+else
+{
+	return (0);
+}
+
+}
+}
