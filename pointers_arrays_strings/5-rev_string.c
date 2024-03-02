9e8385dd1164d5a34356c93bb6653ed7b984349a
Your Name <you@example.com>
2024-03-02 08:45:30 +0000

    asdasd

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - good asdasdasdasda
+ * @s: aisjdaisjdalksjdlkasjdkl
+ * askjdkalksjdkaljsdlkaskd
+ */
+void rev_string(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+while (i >= 1)
+{
+_putchar(s[i - 1]);
+i--;
+}
+}
