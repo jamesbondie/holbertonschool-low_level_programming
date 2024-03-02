88ffd9ebc45405dbea6585643d2413ac0c3c4ca6
Your Name <you@example.com>
2024-03-02 17:04:39 +0000

    asdasd

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcpy - good asdasdasdasda
+ * @dest: aisjdaisjdalksjdlkasjdkl
+ * @src: askjdkalksjdkaljsdlkaskd
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+while (n > i)
+{
+	i++;
+}
+}
