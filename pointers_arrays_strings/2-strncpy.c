dcad5beefb3a7696fd64d14028811b1d98336539
Your Name <you@example.com>
2024-03-02 19:44:21 +0000

    asdasd

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - good asdasdasdasda
+ * @dest: aisjdaisjdalksjdlkasjdkl
+ * @src: askjdkalksjdkaljsdlkaskd
+ * @n: alskdaskldaklsdkalsdaks
+ * Return: alksjdalksjdlkasjdlkajkl
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int lens = 0;
+while (src[lens])
+{
+if (lens < n)
+{
+dest[lens] = src[lens];
+}
+lens++;
+}
+
+
+
+
+return (dest);
+}
