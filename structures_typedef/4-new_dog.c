0dc93d80f6fc2ec5b15b274a8230aa5c017ee4a7
Your Name <you@example.com>
2024-03-02 22:39:20 +0000

    asdasd

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,10 +15,10 @@ dog_t *i;
 int name_len, owner_len;
 int j;
 if (name == NULL || owner == NULL)
-return NULL;
+return (NULL);
 i = malloc(sizeof(dog_t));
 if (i == NULL)
-return NULL;
+return (NULL);
 for (name_len = 0; name[name_len] != '\0'; name_len++);
 for (owner_len = 0; owner[owner_len] != '\0'; owner_len++);
 i->name = malloc(name_len + 1);
@@ -28,7 +28,7 @@ if (i->name == NULL || i->owner == NULL)
 free(i->name);
 free(i->owner);
 free(i);
-return NULL;
+return (NULL);
 }
 for (j = 0; j <= name_len; j++)
 {
@@ -39,5 +39,5 @@ for (j = 0; j <= owner_len; j++)
 i->owner[j] = owner[j];
 i->age = age;
 }
-return i;
+return (i);
 }
