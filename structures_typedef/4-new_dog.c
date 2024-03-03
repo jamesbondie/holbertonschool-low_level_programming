b20cf1cce458c177d70e64369ae1825c6021fd77
Your Name <you@example.com>
2024-03-03 08:20:15 +0000

    configasdasdconfig

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,45 +11,34 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *i;
-int len1 = 0, len2 = 0;
-int j = 0;
+int name_len, owner_len;
+int j;
 if (name == NULL || owner == NULL)
-{
-return NULL;
-}
+return (NULL);
 i = malloc(sizeof(dog_t));
 if (i == NULL)
-{
-return NULL;
-}
-while (name[len1] != '\0')
-{
-len1++;
-}
-while (owner[len2] != '\0')
-{
-len2++;
-}
-i->name = malloc(len1 + 1);
-i->owner = malloc(len2 + 1);
+return (NULL);
+for (name_len = 0; name[name_len] != '\0'; name_len++)
+	;
+for (owner_len = 0; owner[owner_len] != '\0'; owner_len++)
+	;
+i->name = malloc(name_len + 1);
+i->owner = malloc(owner_len + 1);
 if (i->name == NULL || i->owner == NULL)
 {
 free(i->name);
 free(i->owner);
 free(i);
-return NULL;
+return (NULL);
 }
-while (j <= len1)
+for (j = 0; j <= name_len; j++)
 {
 i->name[j] = name[j];
-j++;
 }
-j = 0;
-while (j <= len2)
+for (j = 0; j <= owner_len; j++)
 {
 i->owner[j] = owner[j];
-j++;
-}
 i->age = age;
-return i;
+}
+return (i);
 }
