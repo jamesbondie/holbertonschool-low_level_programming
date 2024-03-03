ebe43de7c59a6ccc20b911571b2a39048a361bb9
Your Name <you@example.com>
2024-03-03 08:19:10 +0000

    configasdasdconfig

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,14 +14,22 @@ dog_t *i;
 int len1 = 0, len2 = 0;
 int j = 0;
 if (name == NULL || owner == NULL)
+{
 return NULL;
+}
 i = malloc(sizeof(dog_t));
 if (i == NULL)
+{
 return NULL;
+}
 while (name[len1] != '\0')
+{
 len1++;
+}
 while (owner[len2] != '\0')
+{
 len2++;
+}
 i->name = malloc(len1 + 1);
 i->owner = malloc(len2 + 1);
 if (i->name == NULL || i->owner == NULL)
