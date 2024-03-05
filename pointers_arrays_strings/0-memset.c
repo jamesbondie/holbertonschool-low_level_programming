87467adcf541b5ba7f7b664ecaba1558b3dce2c5
Your Name <you@example.com>
2024-03-05 06:07:30 +0000

    asdasdas

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strcat - good asdasdasdasda
+ * @dest: aisjdaisjdalksjdlkasjdkl
+ * @src: askjdkalksjdkaljsdlkaskd
+ * Return: alksjdalksjdlkasjdlkajkl
+ */
+char *_memset(char *s, char b, unsigned int n)
+int len = 0;
+
+int len = 0;
+    
+    while (s[len] != 0)
+    {       
+    len++;  
+    }
+    
+    printf("%d", len);
+    
+    int c = 0;
+    while (c <= n)
+    {
+        putchar('.');
+        c++;
+    }
+    
+    while (n<len)
+    {
+        
+        putchar(s[n]);
+        n++;
+    }
+return (*s);
