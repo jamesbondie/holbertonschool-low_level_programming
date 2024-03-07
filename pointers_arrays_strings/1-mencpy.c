b3ff9aeccbee5f14af185bf56317ef0ec35fb4e1
Your Name <you@example.com>
2024-03-07 03:10:40 +0000

    sadasd

---
+++ pointers_arrays_strings/1-mencpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - good asdasdasdasda
+ * @dest: aisjdaisjdalksjdlkasjdkl
+ * @src: askjdkalksjdkaljsdlkaskd
+ * @n: aksjdkjasdjka
+ * Return: alksjdalksjdlkasjdlkajkl
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int c = 0;
+char *blabla = dest;
+while (c <= n)
+{
+*s++ = b;
+c++;
+}
+return (blabla);
+}
