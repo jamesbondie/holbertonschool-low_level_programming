7d95c1a45438b8e6fc52b7d4c0582429195377aa
Your Name <you@example.com>
2024-03-07 03:13:36 +0000

    sadasd

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int c = 0;
-char *blabla = dest;
 while (c <= n)
 {
 *(dest + c) = *(src + c);
 c++;
 }
-return (blabla);
+return (dest);
 }
