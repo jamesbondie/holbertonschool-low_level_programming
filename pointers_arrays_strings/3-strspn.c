48d5a3633bb16d7f2715a8eaaff01bcd8fa56335
Your Name <you@example.com>
2024-03-09 23:22:28 +0000

    hjkhkj

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,18 +11,18 @@ unsigned int _strspn(char *s, char *accept)
 {
 int x = 0;
 int b = 0;
+int c = 0;
 while (s[x] != '\0')
 {
 while (s[b] != ' ')
 {
 if (s[x] == accept[b])
 {
-b++;
+c++;
 }
 b++;		
 }
 x++;
 }
-b--;
-return (b);
+return (c);
 }
