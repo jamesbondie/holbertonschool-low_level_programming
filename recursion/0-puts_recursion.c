f1464b41bd5d34aa1a22a1da72c29b00b2083a6a
Your Name <you@example.com>
2024-03-10 01:12:49 +0000

    asdasd

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * _puts_recursion - check the code
+ * @s: askdasda
+ * sdasd
  * Return: Always 0.
  */
 
 void _puts_recursion(char *s)
-{ 
+{
 if (*s == '\0')
 {
 _putchar('\n');
 return;
-}	
+}
 _putchar(*s);
-_puts_recursion(s + 1); 
+_puts_recursion(s + 1);
 }
