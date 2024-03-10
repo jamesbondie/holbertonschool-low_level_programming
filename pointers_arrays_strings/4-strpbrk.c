d747ab0d672f39828982b7e1144e1a84dfb852a4
Your Name <you@example.com>
2024-03-10 00:01:22 +0000

    hjkhkj

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strspn - good asdasdasdasda
+ * aisjdaisjdalksjdlkasjdkl
+ * @s: askjdkalksjdkaljsdlkaskd
+ * @accept: aksjdkjasdjka
+ * Return: alksjdalksjdlkasjdlkajkl
+ */
+char *_strpbrk(char *s, char *accept)
+{
+    while (*s) {
+        int i = 0;
+        while (accept[i]) {
+            if (*s == accept[i])
+                return s;
+            i++;
+        }
+        s++;
+    }
+    return NULL;
+}
