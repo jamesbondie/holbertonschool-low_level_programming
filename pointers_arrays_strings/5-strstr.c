7f60ddad8a380ef3b506707e8061cdf5e89169bd
Your Name <you@example.com>
2024-03-10 00:11:59 +0000

    hjkhkj

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strpbrk - good asdasdasdasda
+ * aisjdaisjdalksjdlkasjdkl
+ * @s: askjdkalksjdkaljsdlkaskd
+ * @accept: aksjdkjasdjka
+ * Return: alksjdalksjdlkasjdlkajkl
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i = 0, c, j = 0;
+while (haystack[i] != '\0')
+{
+c = i;
+j = 0;
+while (needle[j] != '\0')
+{
+if (haystack[c] == needle[j])
+{
+c++;
+j++;
+}
+else
+{
+break;
+}
+}
+if (needle[j] == '\0')
+{
+return (haystack + i);
+}
+i++;
+}
+return (NULL);
+}
