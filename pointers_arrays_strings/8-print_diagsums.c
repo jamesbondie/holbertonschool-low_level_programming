25ac95b8fadb630ba06348869de100fa6eb8b78b
Your Name <you@example.com>
2024-03-10 00:38:22 +0000

    hjkhkj

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * print_chessboard - good asdasdasdasda
+ * aisjdaisjdalksjdlkasjdkl
+ * @a: askjdkalksjdkaljsdlkaskd
+ * aksjdkjasdjka
+ * Return: alksjdalksjdlkasjdlkajkl
+ */
+void print_diagsums(int *a, int size)
+{
+int x = 0;
+int y = 0;
+int z = 0;
+while (x < size) {
+y = y + a[x];
+x = x + size + 1;
+}
+x = size - 1;
+while (x < size - 1) {
+z = z + a[x];
+x = x + size - 1;
+}
+printf("%d, %d\n", y, z);
+}
