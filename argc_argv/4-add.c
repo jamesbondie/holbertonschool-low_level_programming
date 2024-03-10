e3467798a9b6de01bee88b179e80d6155c3ade43
Your Name <you@example.com>
2024-03-10 03:37:27 +0000

    asdasd

---
+++ argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main -  wdqwdqwdqsdqwdq
+ * @argc: asdasdasdasdt
+ * @argv: arararrarawdwd
+ * Return: 0
+ **/
+
+int main(int argc, char *argv[]) {
+char *arg;
+int j = 0;
+int i = 1
+int sum = 0;
+if (argc == 1) {
+printf("0\n");
+return 0;
+}
+sum = 0;
+for (; i < argc; i++) {
+*arg = argv[i];
+for (; arg[j] != '\0'; j++) {
+if (!isdigit(arg[j])) {
+printf("Error\n");
+return 1;
+}
+}
+sum += atoi(arg);
+}
+printf("%d\n", sum);
+return 0;
+}
