8642c0e1862481cc09451020948a7fc99303a143
Your Name <you@example.com>
2024-03-10 02:15:50 +0000

    asdasd

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -  wdqwdqwdqsdqwdq
+ * @argc: asdasdasdasdt
+ * @argv: arararrarawdwd
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+return 0;
+}
+return (1);
+}
