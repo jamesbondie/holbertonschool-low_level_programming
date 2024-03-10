e59551a941b914a3101c58b04ef20ae5880f085b
Your Name <you@example.com>
2024-03-10 00:40:40 +0000

    hjkhkj

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -12,12 +12,13 @@ void print_diagsums(int *a, int size)
 int x = 0;
 int y = 0;
 int z = 0;
-while (x < size) {
+int d = size * size;
+while (x < d) {
 y = y + a[x];
 x = x + size + 1;
 }
 x = size - 1;
-while (x < size - 1) {
+while (x < d - 1) {
 z = z + a[x];
 x = x + size - 1;
 }
