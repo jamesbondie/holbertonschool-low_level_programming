770bafc1dd43e59c99b526d072e6fe5d18b255c1
Your Name <you@example.com>
2024-03-10 03:19:51 +0000

    asdasd

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -  wdqwdqwdqsdqwdq
+ * @argc: asdasdasdasdt
+ * @argv: arararrarawdwd
+ * Return: 0
+ **/
+
+int main(int argc, char *argv[])
+{
+int i;
+int d;
+int k;
+if (argc <= 2)
+{
+printf("Error\n");
+return (1);
+}
+i = atoi(argv[1]);
+d = atoi(argv[2]);
+k = i * d;
+printf("%d\n", k);
+return (0);
+}
