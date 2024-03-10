75ee660fae3f5b78ccdf2ab7f6863c3bc933f0ac
Your Name <you@example.com>
2024-03-10 02:19:24 +0000

    asdasd

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -  wdqwdqwdqsdqwdq
+ * @argc: asdasdasdasdt
+ * @argv: arararrarawdwd
+ * Return: 0
+ **/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
+
