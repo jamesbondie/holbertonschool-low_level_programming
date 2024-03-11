e87a1d672163257afaf26e62b44f9cdd2a36206c
Your Name <you@example.com>
2024-03-11 18:45:29 +0000

    asdas

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+
+char *array;
+unsigned int i = 0;
+
+if (size == 0)
+{
+return (NULL);
+}
+
+if (size != 0)
+{
+	array = (char*)malloc(sizeof(*char));
+	while (array)
+	{
+		c = array[i]
+			i++;
+	}
+}
+
+
+}
