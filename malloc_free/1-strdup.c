2e36d7b55d88721908d18a11f85bec899df65b17
Your Name <you@example.com>
2024-03-11 19:27:41 +0000

    asdas

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - our main array creater supervisor
+ * @size: asdasdasdasdasdasd
+ * @c: asodjaoisjdaiosjdais
+ * Return: 1 0 0asdoasdasdasd
+ *
+ *
+ *
+ */
+
+char *_strdup(char *str)
+{
+char *pointer;
+
+uzunlugunu tap malloc ele
+
+
+
+
+return (pointer);
+}
