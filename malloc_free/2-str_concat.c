75a5417f6f259a4ca0976d8c40567dde68879d8f
Your Name <you@example.com>
2024-03-12 20:21:01 +0000

    asdasd

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,49 +13,43 @@
 
 char *str_concat(char *s1, char *s2)
 {
-        char *pointer;
-        int len = 0;
-        int len2 = 0;
-        int i = 0;
-        int c = 0;
-      
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	while (s1[len] != '\0')
-        {
-                len++;
-        }
-      
-      
-        while (s2[len2] != '\0')
-        {
-                len2++;
-        }
-        pointer = (char *)malloc(len * sizeof(*s1) + len2 * sizeof(*s2) + 1);
-
-	if (pointer == NULL) 
-	{
-        return (NULL);
-	}
-
-        while (i < len)
-        {
-                pointer[i] = s1[i];
-                i++;
-        }
-
-        while (c < len2)
-        {
-                pointer[i] = s2[c];
-                c++;
-		i++;
-        }
-	pointer[len+len2] = '\0';
-        return (pointer);
+char *pointer;
+int len = 0;
+int len2 = 0;
+int i = 0;
+int c = 0;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+while (s1[len] != '\0')
+{
+len++;
+}
+while (s2[len2] != '\0')
+{
+len2++;
+}
+pointer = (char *)malloc(len * sizeof(*s1) + len2 * sizeof(*s2) + 1);
+if (pointer == NULL)
+{
+return (NULL);
+}
+while (i < len)
+{
+pointer[i] = s1[i];
+i++;
+}
+while (c < len2)
+{
+pointer[i] = s2[c];
+c++;
+i++;
+}
+pointer[len+len2] = '\0';
+return (pointer);
 }
