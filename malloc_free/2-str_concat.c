7686d7ec90e50f43138eb5be766a08cd00f06896
Your Name <you@example.com>
2024-03-12 20:01:00 +0000

    asdasd

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -18,39 +18,18 @@ char *str_concat(char *s1, char *s2)
         int len2 = 0;
         int i = 0;
         int c = 0;
-        if (s1 != NULL)
-        {
+      
         while (s1[len] != '\0')
         {
                 len++;
         }
-        }
+      
 
-	if (s1 == NULL)
-                {
-			len = 0;                       
-  
-		}
-	
-
-        if (s2 != NULL)
-        {
+      
         while (s2[len2] != '\0')
         {
                 len2++;
-        }
-        }
-	
-	if (s2 == NULL)
-                {
-                     len2 = 0;
-                }
-
-	if ( s1 == NULL && s2 == NULL)
-        {
-                return (NULL);
-        }
-
+       }
         pointer = malloc(len * sizeof(*s1) + len2 * sizeof(*s2) + 1);
 
 	if (pointer == NULL) 
