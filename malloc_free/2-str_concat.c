02a33822072a22e5f3711a79d01d8b507053efc7
Your Name <you@example.com>
2024-03-12 18:20:52 +0000

    asdasd

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - our main array creater supervisor
+ * @str: asdasdasdasdasdasd
+ * asodjaoisjdaiosjdais
+ * Return: 1 0 0asdoasdasdasd
+ *
+ *
+ *
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *pointer;
+	int len = 0;
+	int len2 = 0;
+	int i = 0;
+	int c = 0;
+	while (s1[len] != '\0')
+	{
+		len++;
+	}
+	while (s1[len2] != '\0')
+	{
+		len2++;
+	}
+	pointer = malloc(len + len2  + 1);
+	while (i < len)
+	{
+		pointer[i] = s[i];
+		i++;
+	}
+	while (i < len + len2)
+	{
+		pointer[i] = len2[c]
+		i++;
+		c++;
+	}
+	return (pointer);
+}
