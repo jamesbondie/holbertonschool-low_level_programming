42490eefa8a0dae282a4ca3780f188c42903cddb
Your Name <you@example.com>
2024-03-13 18:53:24 +0000

    asdasd

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,46 +2,45 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * str_concat - our main array creater supervisor
- * @s1: asdasdasdasdasdasd
- * @s2: asodjaoisjdaiosjdais
+ * alloc_grid - our main array creater supervisor
+ * @width: asdasdasdasdasdasd
+ * @height: asodjaoisjdaiosjdais
  * Return: 1 0 0asdoasdasdasd
  *
  *
  *
  */
 
-int **alloc_grid(int width, int height) {
-    int **arr;
-    int i, j;
+int **alloc_grid(int width, int height)
+{
+int **arr;
+int i, j;
 int x = width *height;
-
-    if (x <= 0) {
-        return NULL;
-    }
-
-    arr = (int **)malloc(height * sizeof(int *));
-    if (arr == NULL) {
-        return NULL;
-    }
-
-    for (i = 0; i < height; i++) {
-        arr[i] = (int *)malloc(width * sizeof(int));
-        if (arr[i] == NULL)
-	{
-		for(i = 0; i < height; i++)
-		{
-		free(arr[i]);
-		}	
-		free(arr);
-		return (NULL);
-	}
-
-        for (j = 0; j < width; j++) {
-            arr[i][j] = 0;
-        }
-    }
-
-    return arr;
+if (x <= 0)
+{
+return (NULL);
 }
-
+arr = (int **)malloc(height * sizeof(int *));
+if (arr == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+arr[i] = (int *)malloc(width * sizeof(int));
+if (arr[i] == NULL)
+{
+for(i = 0; i < height; i++)
+{
+free(arr[i]);
+}
+free(arr);
+return (NULL);
+}
+for (j = 0; j < width; j++)
+{
+arr[i][j] = 0;
+}
+}
+return (arr);
+}
