b6bcc51bc732e3dc331e4e64e9f8120804764b25
Your Name <you@example.com>
2024-03-13 19:14:47 +0000

    asdasd

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * alloc_grid - our main array creater supervisor
- * @width: asdasdasdasdasdasd
+ * free_grid - our main array creater supervisor
+ * @grid: asdasdasdasdasdasd
  * @height: asodjaoisjdaiosjdais
  * Return: 1 0 0asdoasdasdasd
  *
