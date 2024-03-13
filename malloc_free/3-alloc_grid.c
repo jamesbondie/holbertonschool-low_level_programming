ce9a3c59275630fdc4e728b767fa456f1169d512
Your Name <you@example.com>
2024-03-13 18:13:56 +0000

    asdasd

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,25 +14,28 @@
 #include <stdlib.h>
 
 int **allocate_grid(int width, int height) {
+    int **arr;
+    int i, j;
+
     if (width <= 0 || height <= 0) {
         return NULL;
     }
 
-    int **arr = (int **)malloc(height * sizeof(int *));
+    arr = (int **)malloc(height * sizeof(int *));
     if (arr == NULL) {
         return NULL;
     }
 
-    for (int i = 0; i < height; i++) {
+    for (i = 0; i < height; i++) {
         arr[i] = (int *)malloc(width * sizeof(int));
         if (arr[i] == NULL) {
-            for (int j = 0; j < i; j++) {
+            for (j = 0; j < i; j++) {
                 free(arr[j]);
             }
             free(arr);
             return NULL;
         }
-        for (int j = 0; j < width; j++) {
+        for (j = 0; j < width; j++) {
             arr[i][j] = 0;
         }
     }
