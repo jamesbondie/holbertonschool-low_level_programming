fd65411a7c85e858a0b9414ed82a3fe14644b6e3
Your Name <you@example.com>
2024-03-13 19:01:30 +0000

    asdasd

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * alloc_grid - our main array creater supervisor
+ * @width: asdasdasdasdasdasd
+ * @height: asodjaoisjdaiosjdais
+ * Return: 1 0 0asdoasdasdasd
+ *
+ *
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+if (grid == NULL || height < 0)
+{
+	return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+	free(grid[i]);
+}
+free(grid);
+
+
+}
