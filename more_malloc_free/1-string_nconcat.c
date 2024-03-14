d10b0cfe4e91ad7b7b91495d9e1a27f523ff6598
Your Name <you@example.com>
2024-03-14 06:22:13 +0000

    asdasd

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *pointer;
-int len1 = 0, len2 = 0, len3 = 0, i =0, j = 0;
+int len1 = 0, len2 = 0, len3 = 0, i = 0, j = 0;
 unsigned int b = 0;
 if (s1 == NULL)
 {
