182ea55ce7211013adefb73a634ef035465449bd
Your Name <you@example.com>
2024-03-21 08:48:19 +0000

    asdasd

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * print_name - <F12><F12><F12><F11>.
- * <F10><F10><F11><F11>
- * @name: <F11><F11><F11><F11>
- * @f: <F11><F11><F11><F11><F11><F11><F11><F11>
+ * array_iterator - <F12><F12><F12><F11>.
+ * @array: <F10><F10><F11><F11>
+ * @size: <F11><F11><F11><F11>
+ * @action: <F11><F11><F11><F11><F11><F11><F11><F11>
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
