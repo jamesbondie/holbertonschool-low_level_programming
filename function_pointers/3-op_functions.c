865fba289256455e79ebbec7f0d171e104d19676
Your Name <you@example.com>
2024-03-21 20:59:35 +0000

    asdasd

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -10,33 +10,31 @@
  */
 int op_add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
 int op_sub(int a, int b)
 {
-	return (a - b);
+return (a - b);
 }
 int op_mul(int a, int b)
 {
-	return (a * b);
+return (a * b);
 }
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		
-        printf("Error\n");
-                exit(100);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
 }
-	return (a / b);
+return (a / b);
 }
 int op_mod(int a, int b)
 {
-	if (b == 0)
-        {
-
-        printf("Error\n");
-                exit(100);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
 }
-	return (a %b);
+return (a % b);
 }
