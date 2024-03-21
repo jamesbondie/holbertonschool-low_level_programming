2766fef7e9dfbd07cd514f6827568dfb0c9b84ac
Your Name <you@example.com>
2024-03-21 11:30:02 +0000

    asdasd

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -9,13 +9,12 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i = 0;
-bool x = 0;
 if (size <= 0)
 return (-1);
 while (i < size)
 {
 cmp(array[i]);
-if (cmp(array[i]) != x)
+if (cmp(array[i]) != true)
 	return (i);
 i++;
 }
