c3f35ee2732583e8772d5f900774329aee967c05
Your Name <you@example.com>
2024-03-21 08:28:40 +0000

    asdasd

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,14 +2,12 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 /**
- * init_dog - <F12><F12><F12><F11>.
- * @d: <F10><F10><F11><F11>
+ * print_name - <F12><F12><F12><F11>.
+ * <F10><F10><F11><F11>
  * @name: <F11><F11><F11><F11>
- * @age: <F11><F11><F11><F11>
- * @owner: <F11><F11><F11><F11>
+ * @f: <F11><F11><F11><F11><F11><F11><F11><F11>
  */
 void print_name(char *name, void (*f)(char *))
 {
-
 f(name);
 }
