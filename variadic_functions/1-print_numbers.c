9ed4d6a2cf75276abd9e340e4f3750da27f980d1
Your Name <you@example.com>
2024-03-22 05:19:03 +0000

    asdasd

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdarg.h>
+/**
+ * print_numbers - my beautiful function
+ * @separator: okai okai, lemme guess how long i am
+ * @n: oaisdjaoisjdoaisjdoiasjdioj
+ * Return: asdaskdahsjdasjkd
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i = 0;
+va_list x;
+va_start(x, n);
+while (i < n)
+printf(va_arg(x, unsigned int);
+printf(separator);
+i++;
+}
+va_end(x);
+}
