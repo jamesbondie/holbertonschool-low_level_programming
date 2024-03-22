461df43b0ded68fc984012eca1fc6eaf31fda148
Your Name <you@example.com>
2024-03-22 05:39:02 +0000

    asdasd

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - my beautiful function
+ * @separator: okai okai, lemme guess how long i am
+ * @n: oaisdjaoisjdoaisjdoiasjdioj
+ * Return: asdaskdahsjdasjkd
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int i = 0;
+va_list args;
+va_start(args, n);
+while (i < n)
+{
+if (n !=  NULL)
+printf("%d", va_arg(args, int));
+if (n == NULL)
+printf("nil");
+if (i < n - 1)
+{
+if (separator != NULL)
+printf("%s", separator);
+}
+i++;
+}
+printf("\n");
+va_end(args);
+}
