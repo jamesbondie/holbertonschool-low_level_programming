aa63c7195ca2828423257e897cdf3befbd6b6d0c
root <jamesalfreddie@gmail.com>
2024-04-06 18:20:37 +0000

    betty fixed

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -6,7 +6,7 @@
 /**
  * free_list - it adds the given string to the head of our linked list
  * @head: it is our head
- *  and this is the input string for addition to head
+ * and this is the input string for addition to head
  * Return: in this particular function it returns the address of last element
  */
 
@@ -14,12 +14,10 @@
 
 void free_list(list_t *head)
 {
-	if (head != NULL)
-	{
+if (head != NULL)
+{
 if (head->next != NULL)
 free_list(head->next);
-
-
 free(head->str);
 free(head);
 }
