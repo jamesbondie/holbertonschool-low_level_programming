9154cee662420b035d170e63cc28a48c3367c9b7
root <jamesalfreddie@gmail.com>
2024-04-06 20:26:32 +0000

    i hated that

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,3 +1,21 @@
+/**
+* binary_to_uint - it converts binary to unsignet int
+* @b: char b
+*
+* Return: it gives us good results
+*/
 unsigned int binary_to_uint(const char *b)
 {
-
+unsigned int z = 0;
+int i;
+if (b == NULL)
+return (0);
+while (*b != '\0')
+{
+if (*b != '0' && *b != '1')
+return (0);
+z = z * 2 + *b - '0';
+b = b + 1;
+}
+return (z);
+}
