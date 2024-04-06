c894036cc9b557a644ae21474e72c434e2c00848
root <jamesalfreddie@gmail.com>
2024-04-06 17:53:13 +0000

    free list created

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - it adds the given string to the head of our linked list
+ * @head: it is our head
+ *  and this is the input string for addition to head
+ * Return: in this particular function it returns the address of last element
+ */
+
+
+
+void free_list(list_t *head)
+{
+list_t iter = head;
+
+while (iter != NULL)
+{
+iter = iter->next;
+free(head);
+head = iter;
+}
+return (head);
+}
