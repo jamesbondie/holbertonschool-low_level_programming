3bde92931c3a138a3ce288c715b62bfb0930a63d
root <jamesalfreddie@gmail.com>
2024-04-07 08:19:34 +0000

    sorry i misunderstood

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -9,28 +9,15 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-dlistint_t *temp, *iter;
+dlistint_t *iter;
 unsigned int x = 0;
-temp = malloc(sizeof(head));
-if (temp == NULL)
-return (NULL);
-temp->prev = NULL;
-temp->n = n;
-temp->next = NULL;
-if (*head == NULL)
-{
-temp->prev = NULL;
-*head = temp;
-return (temp);
-}
 while (x < 5)
 {
 	if (iter->next == NULL)
 		iter = iter->next;
 }
-iter->next = temp;
-temp->prev = iter;
+iter->next = head;
+head->prev = iter;
 
-return (temp);
-
+return (iter);
 }
