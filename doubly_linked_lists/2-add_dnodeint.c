83c19d76c2c398cd63b39e751226d9e7ffb68552
root <jamesalfreddie@gmail.com>
2024-04-07 07:46:59 +0000

    error fixed

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,12 +11,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *temp;
 temp = malloc(sizeof(dlistint_t));
+if (temp == NULL)
+return (NULL);
 temp->prev = NULL;
 temp->n = n;
 temp->next = NULL;
 temp->next = *head;
+if (*head != NULL)
 (*head)->prev = temp;
 *head = temp;
 return (temp);
-
 }
