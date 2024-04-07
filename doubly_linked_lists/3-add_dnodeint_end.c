25d623276b7fbcb0daa67d883c9120cae59ba2bb
root <jamesalfreddie@gmail.com>
2024-04-07 08:01:48 +0000

    changes made for iteration and head empty situation

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,16 +9,26 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-dlistint_t *temp;
+dlistint_t *temp, *iter;
+iter = *head;
 temp = malloc(sizeof(dlistint_t));
 if (temp == NULL)
 return (NULL);
 temp->prev = NULL;
 temp->n = n;
 temp->next = NULL;
-temp->prev = *head;
-if (*head != NULL)
-(*head)->next = temp;
+if (*head == NULL)
+{
+temp->prev = NULL;
 *head = temp;
+return (temp);
+}
+
+while (iter->next != NULL)
+{
+iter = iter->next;
+}
+iter->next = temp;
+temp->prev = iter;
 return (temp);
 }
